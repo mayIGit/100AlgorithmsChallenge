b0a1480a7b8342948194dbd648ad94d9bb9f534a
Meghana Rawate <meghana.rawate@gmail.com>
2021-01-04 16:12:18 -0500

    Solution to absoluteValueSumMinimization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,6 +1,23 @@
 function absoluteValuesSumMinimization(a: number[]): number {
 
+    //Does the array contain even or odd number of values
+    const isEven = a.length % 2 === 0 ? true : false;
+    if(!isEven){
+        //if odd then find the central value and return it
+        return a[Math.floor(a.length/2)]
+
+    }
+    else {
+        //if even, then split the array into two, return the left value
+        return  a[a.length/2 - 1]
+    }
+
 }
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
 
 // console.log(absoluteValuesSumMinimization([2, 4, 7]));
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
