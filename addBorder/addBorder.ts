fdd7b36adbf23176baf249496eac6c6c7ba83782
Meghana Rawate <meghana.rawate@gmail.com>
2021-01-05 19:47:59 -0500

    Solved AddBorder Challenge

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
 function addBorder(picture: string[]): string[] {
+  const len = picture[0].length;
+  let stars = '';
+  for (let i = 0; i < len; i++) {
+    stars += '*';
+  }
+  picture.push(stars);
+  picture.unshift(stars);
 
+  for (let i = 0; i < picture.length; i++) {
+    picture[i] = '*'.concat(picture[i], '*');
+  }
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']));
