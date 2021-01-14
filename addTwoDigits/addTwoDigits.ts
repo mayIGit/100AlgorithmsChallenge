970d9ac59f6bc7684e6cd2cb366c9611803e78a9
Meghana Rawate <meghana.rawate@gmail.com>
2021-01-14 06:40:20 -0500

    Adding digits of a number

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
 function addTwoDigits(n: any): number {
+  //take the number passed in and convert it to an array of characters
+  let arr = n.toString().split('');
+  console.log(arr);
 
+  //now add the split values inside the array using the reduce method. Since the two are strings, we need to parseInt them
+  //to be able to add them
+  const result = arr.reduce((a, acc) => {
+    return parseInt(a) + parseInt(acc);
+  });
+  return result;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
